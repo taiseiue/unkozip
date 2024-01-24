4f0b66d3e6565a2ce67b05c26a733cb72afd1ec9
taiseiue <taisei@wsoft.ws>
2024-01-24 20:44:36 +0900

    プロジェクト ファイルを追加します。

---
+++ unkozip/Options.cs
@@ -0,0 +1,22 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unkozip
+{
+    internal class Options
+    {
+        [Value(0,MetaName ="Zipファイル名")]
+        public string Source { get; set; }
+        [Option('o',"out", Required =false, HelpText ="解凍先ディレクトリ")]
+        public string Output { get; set; }
+        [Option("mcp",Required =false,Default =65001,HelpText ="解凍に使用する文字コードのコードポイント(数値)")]
+        public int CodePoint { get; set; }
+
+        [Option('c',"code", Required = false, HelpText = "解凍に使用する文字コードの名前")]
+        public string CodeName { get; set; }
+    }
+}
---
+++ unkozip/Program.cs
@@ -0,0 +1,41 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace unkozip
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var options = Parser.Default.ParseArguments<Options>(args);
+                options.WithNotParsed(_ =>
+                {
+                    var helpText = HelpText.AutoBuild(options);
+                    Console.WriteLine($"引数を認識できませんでした: {helpText}");
+                    Environment.Exit(255);
+                });
+
+                string source = options.Value.Source;
+                string output = string.IsNullOrEmpty(options.Value.Output) ? Path.Combine(Directory.GetCurrentDirectory(), Path.GetFileNameWithoutExtension(source)) : options.Value.Output;
+                Encoding encoding = string.IsNullOrEmpty(options.Value.CodeName)
+                    ? Encoding.GetEncoding(options.Value.CodePoint)
+                    : Encoding.GetEncoding(options.Value.CodeName);
+
+                Console.WriteLine("Zipファイルを解凍しています...");
+                ZipFile.ExtractToDirectory(source, output, encoding);
+                Console.WriteLine("成功");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"失敗:{ex.Message}");
+                Environment.Exit(255);
+            }
+        }
+    }
+}
